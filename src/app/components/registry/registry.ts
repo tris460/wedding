65350a8a4f4858f1a22816567fe0bece03f51b21
Beatriz Martinez Perez <beatriz.martinez@xaldigital.com>
2026-05-20 13:58:28 -0600

    add payal as pay method

--- src/app/components/registry/registry.ts
+++ src/app/components/registry/registry.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '../../pipes/translate.pipe';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registry',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, CommonModule],
   templateUrl: './registry.html',
   styleUrl: './registry.scss'
 })
@@ -13,17 +14,24 @@ export class Registry {
   
   cards = [
     {
-      holder: 'Beatriz Martinez Pérez',
+      holder: 'Beatriz Martínez Pérez',
       number: '4152 3142 8496 9972',
       bankImage: 'logos/bbva.png',
       bankName: 'BBVA'
     },
     {
       holder: 'Erick Alejandro Plácido Vázquez',
-      number: '4189 1431 4826 8082',
+      number: '4189 1431 4950 9831',
       bankImage: 'logos/banorte.png',
       bankName: 'Banorte'
-    }
+    },
+    {
+      holder: 'Beatriz Martínez Pérez',
+      number: null,
+      bankImage: 'logos/paypal.png',
+      bankName: 'PayPal',
+      link: 'https://www.paypal.com/paypalme/BeatrizMtzP'
+    },
   ];
   
   showCopyMessage = false;
